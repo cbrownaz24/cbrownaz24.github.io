deddbfad8bef2004a7d2e97575ef4660c95fcf89
cbrownaz24 <connorbrown059@gmail.com>
2024-06-20 21:47:08 -0600

    updated content

--- index.js
+++ index.js
@@ -59,32 +59,6 @@ window.onclick = function(event) {
   }
 }
 
-//BUTTON 3
-var modal3 = document.getElementById("modal3");
-
-// Get the button that opens the modal
-var btnModal3 = document.getElementById("btn3");
-
-// Get the <span> element that closes the modal
-var spanModal3 = document.getElementsByClassName("closeModal3")[0];
-
-// When the user clicks on the button, open the modal
-btnModal3.onclick = function() {
-  modal3.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-spanModal3.onclick = function() {
-  modal3.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal3) {
-    modal3.style.display = "none";
-  }
-}
-
 //HOBBY MODALS
 
 //BUTTON HOBBY 1
